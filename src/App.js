033770997c900056ad41d499546056cfb62a8546
vipin bansal <vipinbansal@vipins-MacBook-Air.local>
2022-04-14 17:27:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDoApplication from "./Project_comp/todo";
+
+function App() {
+  return (
+    <div>
+      <ToDoApplication/>
+    </div>
+  );
+}
+
+export default App;
