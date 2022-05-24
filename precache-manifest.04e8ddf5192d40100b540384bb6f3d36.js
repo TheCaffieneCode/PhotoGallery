5e65d3da30711e9c7c52a0dbdf15ccdc37ed464c
vipin bansal <vipinbansal@vipins-MacBook-Air.local>
2022-05-24 13:09:19 +0530

    Updates

---
+++ precache-manifest.04e8ddf5192d40100b540384bb6f3d36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cccb4629b6fbe625179d",
+    "url": "/PhotoGallery/static/css/main.8fc4b7b9.chunk.css"
+  },
+  {
+    "revision": "cccb4629b6fbe625179d",
+    "url": "/PhotoGallery/static/js/main.cccb4629.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/PhotoGallery/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "153f77b5479057200d4c",
+    "url": "/PhotoGallery/static/js/2.153f77b5.chunk.js"
+  },
+  {
+    "revision": "285b7f9a0a8c682186d6",
+    "url": "/PhotoGallery/static/js/runtime~main.285b7f9a.js"
+  },
+  {
+    "revision": "63003ac4690522da8571d37c7879be68",
+    "url": "/PhotoGallery/static/media/Cat.63003ac4.jpeg"
+  },
+  {
+    "revision": "bb92e702666ea7fb08b6371a8b11040b",
+    "url": "/PhotoGallery/static/media/dog.bb92e702.jpeg"
+  },
+  {
+    "revision": "e9116a8790f17096ce0ae33ff9b3b435",
+    "url": "/PhotoGallery/static/media/hawk.e9116a87.jpeg"
+  },
+  {
+    "revision": "0bba365d2e5c753e85c90c1cef71563e",
+    "url": "/PhotoGallery/static/media/koel.0bba365d.jpeg"
+  },
+  {
+    "revision": "8f0c085eacb04f598d19798e3622e179",
+    "url": "/PhotoGallery/static/media/lion.8f0c085e.jpeg"
+  },
+  {
+    "revision": "857426cfcc4fb34789c05cd767005145",
+    "url": "/PhotoGallery/static/media/owl.857426cf.webp"
+  },
+  {
+    "revision": "c0e09ea39e482cde98cd57be39c50831",
+    "url": "/PhotoGallery/static/media/parrot.c0e09ea3.jpeg"
+  },
+  {
+    "revision": "ca0856b01e604f76f4494503fce1ce64",
+    "url": "/PhotoGallery/static/media/wolf.ca0856b0.jpeg"
+  },
+  {
+    "revision": "de9956ce07c00cd0cf7a947e90444162",
+    "url": "/PhotoGallery/index.html"
+  }
+];
